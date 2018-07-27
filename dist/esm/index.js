3ba5b42962c67b515db299d502e4fda4ac34edb0
Lucien Bénié <lucien.benie@gmail.com>
2018-07-26 22:46:14 -0400

    release 0.1.0

---
+++ dist/esm/index.js
@@ -0,0 +1,58 @@
+const en = {
+    less: 'less than a minute read',
+    default: 'min. read'
+};
+
+const fr = {
+    less: `moins d'une minute de lecture`,
+    default: 'min. de lecture'
+};
+
+const es = {
+    less: `menos de un minuto leyendo`,
+    default: 'min. leer'
+};
+
+const translations = {
+    en,
+    fr,
+    es
+};
+const hasTranslation = (language) => Object.keys(translations).some(x => x === language);
+
+const defaultOpts = {
+    wordsPerMinute: 200,
+    locale: 'en'
+};
+const readingTime = (data, opts) => {
+    if (data == null) {
+        throw new Error('Data provided is invalid');
+    }
+    const words = data.match(/[\w\d\s,.\u00C0-\u024F]+/gi);
+    const options = Object.assign({}, defaultOpts, opts);
+    if (words == null || words.length === 0) {
+        throw new Error('Data provided is invalid');
+    }
+    if (options.wordsPerMinute == null || options.wordsPerMinute === 0) {
+        throw new Error(`Please provide a 'wordPerMinute' option greater than 0`);
+    }
+    if (options.locale == null || options.locale.length === 0) {
+        throw new Error(`Please provide a 'locale' option`);
+    }
+    const minutes = words[0].length / options.wordsPerMinute;
+    const time = minutes * 60 * 1000;
+    const displayedTime = Math.round(minutes);
+    const hasLocale = hasTranslation(options.locale);
+    let text = `${displayedTime} ${hasLocale ? translations[options.locale].default : translations.en.default}`;
+    if (displayedTime < 1.0001) {
+        text = hasLocale ? translations[options.locale].less : translations.en.less;
+    }
+    return {
+        text,
+        minutes,
+        time,
+        words: words[0].length
+    };
+};
+
+export { readingTime };
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const input = 'lib/index.ts';
@@ -13,17 +12,8 @@ export default {
     {
       file: pkg.main,
       format: 'iife',
-      name,
-      plugins: [
-        terser({
-          ecma: 6,
-          output: {
-            comments: true
-          }
-        })
-      ]
+      name
     },
-    { file: 'dist/browser/index.js', format: 'iife', name },
     { file: 'dist/cjs/index.js', format: 'cjs', name },
     { file: pkg.module, format: 'es' }
   ],
