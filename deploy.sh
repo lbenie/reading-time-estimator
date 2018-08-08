7cdd7b17be36dca51f7b3227ea1fa1d7b3581d9e
Lucien Bénié <lucien.benie@gmail.com>
2018-08-08 19:07:06 -0400

    ci(travis build docs): generate docs

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:lbenie/reading-time-estimator.github.io.git master
+git push -f git@github.com:lbenie/reading-time-estimator.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
