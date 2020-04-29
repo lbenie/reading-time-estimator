8437608075658e633f34424ae144ea310a471db4
Lucien Bénié <lucien.benie@logmein.com>
2020-04-29 14:01:57 -0400

    fix: unit tests

--- __tests__/reading-time-estimation.spec.ts
+++ __tests__/reading-time-estimation.spec.ts
@@ -7,6 +7,8 @@ const chineseText = `春眠不觉晓，处处闻啼鸟。`
 
 const frenchText = `Reading Time Estimator a été créé pour fournir une estimation de la durée de lecture d'un article ou d'un blog comme vu sur medium`
 
+const spanishText = `Reading Time Estimator fue creado para proporcionar una estimación del tiempo de lectura de un artículo o blog como se ve en el medio`
+
 describe('readingTime', () => {
   it.each`
     language                 | words          | wordsPerMinute | expectedResult
@@ -16,6 +18,7 @@ describe('readingTime', () => {
     ${undefined}             | ${undefined}   | ${undefined}   | ${{ minutes: 0, words: 0, text: translations[SupportedLanguages.EN].less }}
     ${SupportedLanguages.CN} | ${chineseText} | ${2}           | ${{ minutes: 5, words: 10, text: `5 ${translations[SupportedLanguages.CN].default}` }}
     ${SupportedLanguages.FR} | ${frenchText}  | ${10}          | ${{ minutes: 3, words: 26, text: `3 ${translations[SupportedLanguages.FR].default}` }}
+    ${SupportedLanguages.EN} | ${spanishText} | ${10}          | ${{ minutes: 2, words: 24, text: `2 ${translations[SupportedLanguages.EN].default}` }}
   `(
     'approximates time to read a text in $language',
     ({
