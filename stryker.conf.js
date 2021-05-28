a0be4ba46cbd3dac41564d4f0ef0bc7f9fa668bd
Lucien Bénié <lucien.benie@logmein.com>
2021-05-28 12:37:40 -0400

    chore: deps

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,18 +1,14 @@
-module.exports = function(config) {
-  config.set({
-    testRunner: 'jest',
-    testFramework: 'jest',
-    mutator: 'typescript',
-    reporter: ['clear-text', 'progress', 'dashboard'],
-    packageManager: 'yarn',
-    coverageAnalysis: 'off',
-    tsconfigFile: 'tsconfig.json',
-    maxConcurrentTestRunners: 8,
-    mutate: ['lib/*.ts'],
-    jest: {
-      project: 'default',
-      config: require(__dirname + '/jest.config.js')
-    },
-    timeoutMs: 100000
-  })
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  packageManager: 'yarn',
+  reporters: ['html', 'clear-text', 'progress', 'dashboard'],
+  testRunner: 'jest',
+  coverageAnalysis: 'all',
+  tsconfigFile: 'tsconfig.spec.json',
+  mutate: ['lib/index.ts'],
+  jest: {
+    configFile: 'jest.config.js',
+  },
 }
