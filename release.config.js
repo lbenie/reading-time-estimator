7d518503afcb51e482f07ef86577fd63f8d0c06a
Lucien Bénié <lbenie@users.noreply.github.com>
2021-12-18 16:41:29 +0000

    chore: updating release config file

--- release.config.js
+++ release.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   branches: [
-    'master',
     'main',
     'next',
     {
@@ -12,5 +11,60 @@ module.exports = {
       prerelease: true,
     },
   ],
-  plugins: ['@semantic-release/changelog', '@semantic-release/git'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'eslint',
+        releaseRules: [
+          { type: 'docs', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'chore', scope: 'deps', release: 'patch' },
+        ],
+        parserOpts: {
+          noteKeywords: [
+            'BREAKING CHANGE',
+            'BREAKING CHANGES',
+            'BREAKING',
+            'breaking change',
+            'breaking changes',
+            'breaking',
+          ],
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'eslint',
+        parserOpts: {
+          noteKeywords: [
+            'BREAKING CHANGE',
+            'BREAKING CHANGES',
+            'BREAKING',
+            'breaking change',
+            'breaking changes',
+            'breaking',
+          ],
+        },
+        writerOpts: {
+          commitsSort: ['subject', 'scope'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+  ],
 }
