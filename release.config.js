bdcf6875d83ab90319aa32cb725915f86e24d08a
Lucien Bénié <lbenie@users.noreply.github.com>
2022-01-26 15:51:31 -0500

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -19,7 +19,7 @@ module.exports = {
         releaseRules: [
           { type: 'docs', release: 'patch' },
           { type: 'refactor', release: 'patch' },
-          { type: 'chore', scope: 'deps', release: 'patch' },
+          // { type: 'chore', scope: 'deps', release: 'patch' },
         ],
         parserOpts: {
           noteKeywords: [
