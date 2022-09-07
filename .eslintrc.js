a7966f182f3b3e88fbe358e7831fe2888f84095e
Lucien Bénié <lbenie@users.noreply.github.com>
2022-09-07 11:50:07 -0400

    feat: export user type (#707)
    * feat: export supported languages type

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,18 +5,15 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'prettier/prettier': ['error', { 'singleQuote': true, 'semi': false }],
+    'prettier/prettier': ['error', { singleQuote: true, semi: false }],
     '@typescript-eslint/indent': ['error', 2],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/member-delimiter-style': 'off',
-    '@typescript-eslint/indent': 'off'
+    '@typescript-eslint/indent': 'off',
   },
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
-  plugins: [
-    'prettier',
-    '@typescript-eslint'
-  ]
+  plugins: ['prettier', '@typescript-eslint'],
 }
