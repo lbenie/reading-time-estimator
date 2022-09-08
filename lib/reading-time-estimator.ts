2af33988506d84dea39146d516b68d2d72b28f86
Lucien Bénié <lbenie@users.noreply.github.com>
2022-09-08 17:19:03 +0000

    docs: adds typedoc to functions and interfaces

--- lib/reading-time-estimator.ts
+++ lib/reading-time-estimator.ts
@@ -1,13 +1,26 @@
 import { translations } from './i18n'
 import type { SupportedLanguages } from './i18n'
 
-interface ReadingTime {
+/**
+ * Reading time estimator result shape
+ */
+type ReadingTime = {
+  /**
+   * Number of minutes to read the text
+   */
   readonly minutes: number
+  /**
+   * Number of words in the text
+   */
   readonly words: number
+  /**
+   * Localized message with the number of minutes to read the text
+   */
   readonly text: string
 }
+
 /**
- *
+ * Parses the text and returns an array of words
  * @param data - The text to be analyzed
  * @returns Parsed chinese, japanese and accented text
  */
@@ -15,6 +28,11 @@ const parseWords = (data: string) =>
   data.match(/[\w|\d|\s|,|.|\u00C0-\u024F|\u4E00-\u9FA5|\u3041-\u309F]+/giu) ??
   []
 
+/**
+ * Calculates the number of words in the text
+ * @param data - The text to be analyzed
+ * @returns number of words in the text
+ */
 const getNumberOfWords = (data: string) =>
   parseWords(data).reduce(
     (accumulator, word) =>
@@ -23,8 +41,19 @@ const getNumberOfWords = (data: string) =>
     0
   )
 
+/**
+ * Checks if the number of minutes is less than 1
+ * @param minutes - Number of minutes to read the text
+ * @returns True if the number of minutes is less than 1, otherwise false
+ */
 const isLessThanAMinute = (minutes: number) => minutes < 1 + Number.EPSILON
 
+/**
+ * Grabs the correct translation
+ * @param minutes - Number of minutes to read the text
+ * @param locale - Locale to be used in the result
+ * @returns localized message with the number of minutes to read the text
+ */
 const getLocale = (minutes: number, locale: SupportedLanguages = 'en') =>
   translations[locale][isLessThanAMinute(minutes) ? 'less' : 'default']
 
@@ -41,7 +70,7 @@ export const readingTime = (
   language: SupportedLanguages = 'en'
 ): ReadingTime => {
   const words = getNumberOfWords(data)
-  const minutes = +Math.round(words / wordsPerMinute).toFixed(2)
+  const minutes = Number(Math.round(words / wordsPerMinute).toFixed(2))
 
   return {
     minutes,
