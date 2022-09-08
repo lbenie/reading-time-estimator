9ceb6f9cae75df312ae61746dccb3a8c2a684ef8
Lucien Bénié <lbenie@users.noreply.github.com>
2022-09-08 13:26:52 -0400

    docs: adds typedoc (#709)
    * docs: adds typedoc to functions and interfaces

--- lib/i18n/supportedLanguages.ts
+++ lib/i18n/supportedLanguages.ts
@@ -1,3 +1,9 @@
+/**
+ * Supported languages of the library
+ */
 export const supportedLanguages = ['en', 'fr', 'es', 'cn', 'ja', 'de'] as const
 
+/**
+ * Supported languages types of the library
+ */
 export type SupportedLanguages = typeof supportedLanguages[number]
