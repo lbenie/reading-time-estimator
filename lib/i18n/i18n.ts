8999f0d820c0baec6cd717341e620bf43a4a45c3
Lucien Bénié <lbenie@users.noreply.github.com>
2022-09-08 17:22:50 +0000

    docs: adds typedoc to functions and interfaces

--- lib/i18n/i18n.ts
+++ lib/i18n/i18n.ts
@@ -1,4 +1,13 @@
-export interface I18n {
+/**
+ * Localization object shared by all locales
+ */
+export type I18n = {
+  /**
+   * Translation for less than a minute
+   */
   less: string
+  /**
+   * Translation for more than a minute
+   */
   default: string
 }
