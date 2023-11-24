1b2a8317bd8dc94e5434baac52fcb51a70026119
Lucien Bénié <lucien.benie@gmail.com>
2023-11-24 10:19:04 -0500

    chore: use my personal linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,3 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 13,
-    sourceType: 'module',
-  },
-  rules: {
-    'prettier/prettier': ['error', { singleQuote: true, semi: false }],
-    '@typescript-eslint/indent': ['error', 2],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/member-delimiter-style': 'off',
-    '@typescript-eslint/indent': 'off',
-  },
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['prettier', '@typescript-eslint'],
+  extends: [require.resolve('@lbenie/linting/eslint')],
 }
